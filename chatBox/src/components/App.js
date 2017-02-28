fdfc93655010711f768dfdb19fef99cff8f75a3c
PERNIN Frédéric <pernin_f@epitech.eu>
2017-02-28 22:50:03 +0100

    submit exo 2

--- chatBox/src/components/App.js
+++ chatBox/src/components/App.js
@@ -5,6 +5,9 @@ import base from '../database';
 import Formulaire from './Formulaire';
 import Message from './Message';
 
+import ReactCSSTransistionGroup from 'react-addons-css-transition-group';
+import '../animation.css';
+
 class App extends React.Component{
     state = {
       messages: {}
@@ -17,6 +20,11 @@ class App extends React.Component{
         });
     }
 
+    componentDidUpdate(){
+        //Mettre le scroll en bas
+        this.messages.scrollTop = this.messages.scrollHeight;
+    }
+
 
 
 
@@ -26,25 +34,42 @@ class App extends React.Component{
         //Ajouter une clé unique timestamp
         const timestamp = Date.now();
         messages[`message-${timestamp}`] = message;
+        //On supprime si plus de 10 messages
+        Object.keys(messages).slice(0, -10).map(key => messages[key] = null);
         //Mettre à jour le state
         this.setState({messages});
+    };
+
+    isUser = pseudo => {
+      return pseudo === this.props.params.pseudo
     };
 
     render(){
         const messages = Object
             .keys(this.state.messages)
-            .map(key => <Message key={key} details={this.state.messages[key]}/>);
+            .map(key => <Message key={key} isUser={this.isUser} details={this.state.messages[key]}/>);
 
         return(
             <div className="box">
                 <div>
-                    <div className="messages">
+                    <div className="messages" ref={input => this.messages = input}>
+                        <ReactCSSTransistionGroup
+                            component="div"
+                            className="message"
+                            transitionName="message"
+                            transitionEnterTimeOut={200}
+                            transitionLeaveTimeOut={200}
+                        >
                         {messages}
+                        </ReactCSSTransistionGroup>
                     </div>
-                    <Formulaire pseudo={this.props.params.pseudo} length="140" addMessage={this.addMessage}/>
+                    <Formulaire pseudo={this.props.params.pseudo} length={140} addMessage={this.addMessage}/>
                 </div>
             </div>
         )
+    }
+    static propTypes = {
+        params: React.PropTypes.object.isRequired,
     }
 }
 export default App;
